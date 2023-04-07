ba9cc0d7d8d44d519b4c3abced693800b4933b8e
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-07 01:53:44 +0000

    Rock page playing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+    render({:template => "game_templates/user_rock.html.erb"})
+    #self.redirect_to("https://www.wikipedia.org")
+  end
+
+
 
 end
